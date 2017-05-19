dc033108c084591f7bcab42fdbfcef4c0e4eafa0
Jesus Jr <jesuscanezjr@gmail.com>
2017-05-18 21:02:47 -0700

    first commit

---
+++ factoryPattern.Cli/factoryPattern.Cli/Model/Vehicle/Automotive/Automobile.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace factoryPattern.Cli.Model.Vehicle.Automotive
+{
+    class Automobile
+    {
+    }
+}
---
+++ factoryPattern.Cli/factoryPattern.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace factoryPattern.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
