a18312a99aed60cd653fd89e146023489dd851c1
Jesus Jr <jesuscanezjr@gmail.com>
2017-05-18 21:15:21 -0700

    enum for types

---
+++ factoryPattern.Cli/factoryPattern.Cli/Model/Domain/Vehicle/VehicleTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace factoryPattern.Cli.Model.Domain.Vehicle
+{
+    public enum VehicleTypes
+    {
+        Automotive, 
+        Plane, 
+        Boat
+    }
+}
